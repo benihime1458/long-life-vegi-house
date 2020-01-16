667792e4284fc362cd54412458b40450b5167bc3
baklavacado <benny.l.wu@gmail.com>
2020-01-16 15:07:59 -0800

    first commit

---
+++ js/init.js
@@ -0,0 +1,8 @@
+(function($){
+  $(function(){
+    $('.carousel').carousel();
+    $('.parallax').parallax();
+    $('.materialboxed').materialbox();
+    $('.modal').modal();
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
